65b9cc3bb8fbea917ba8987bf4e28ac40445a82e
jfitzinger <jfitzinger@gmail.com>
2014-06-09 17:46:35 +0100

    Removed bean output

--- src/main/java/bootiful/Application.java
+++ src/main/java/bootiful/Application.java
@@ -1,10 +1,7 @@
 package bootiful;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,16 +11,8 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
 
-        
-        System.out.println("Beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
     }
 
 }
