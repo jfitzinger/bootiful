77e2063d7a00e74fbf2ef486fac8b34dcf484770
jfitzinger <jfitzinger@gmail.com>
2014-06-12 19:10:38 +0100

    added reformating of heroku DB url to match jdbc syntax

--- src/main/java/bootiful/Application.java
+++ src/main/java/bootiful/Application.java
@@ -1,5 +1,8 @@
 package bootiful;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,10 +13,31 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-        System.out.println("app started");
+	private static final String DATABASE_URL_PROPERTY = "database.url";
 
-    }
+	public static void main(String[] args) throws URISyntaxException {
+		checkAndCorrectDatabaseUrl();
+		SpringApplication.run(Application.class, args);
+	}
+
+	private static void checkAndCorrectDatabaseUrl() throws URISyntaxException {
+		String url = System.getProperty(DATABASE_URL_PROPERTY);
+		if (url!=null && isHerokuPostgres(url)) {
+			System.setProperty(DATABASE_URL_PROPERTY, reformatHerokuPostgresUrl(new URI(url)));
+		}
+	}
+
+	private static boolean isHerokuPostgres(String url) {
+		return url.startsWith("postgres");
+	}
+
+	private static String reformatHerokuPostgresUrl(URI url) {
+		StringBuffer correctUrl = new StringBuffer("jdbc:postgresql://");
+		correctUrl.append(url.getHost()).append(':').append(url.getPort());
+		correctUrl.append(url.getPath()).append("?username=")
+				.append(url.getUserInfo().split(":")[0]);
+		correctUrl.append("?password=").append(url.getUserInfo().split(":")[1]);
+		return correctUrl.toString();
+	}
 
 }
